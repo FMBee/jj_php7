e5d77c596d50e076eb293c8edad1c45b29212c92
FMBee <33802035+FMBee@users.noreply.github.com>
2019-11-06 17:31:11 +0100

    si entree, defines

--- config/defines.php
+++ config/defines.php
@@ -1,22 +1,26 @@
-<?php 
-	
+<?php
+
 	// Arborescence application
-	
-	$_SEP = '\\'.DIRECTORY_SEPARATOR; 	// !!:vaut '\' sous Windows et '/' sous Linux
-	$_ROOT = '.'.$_SEP;				// racine pour include PHP
+
+    if ( strtoupper(substr(PHP_OS, 0, 3)) == 'WIN' )
+        $_SEP = '\\'.DIRECTORY_SEPARATOR;
+    else
+        $_SEP = '/'.DIRECTORY_SEPARATOR;
+
+    $_ROOT = '.'.$_SEP;				// racine pour include PHP
 
 	if (!defined('__PATH_SEPARATOR__'))     	define('__PATH_SEPARATOR__'			, $_SEP );
 	if (!defined('__DOCUMENT_ROOT__'))     		define('__DOCUMENT_ROOT__'			, $_ROOT );
-	
+
 	if (!defined('__CONTROLLERS_ROOT__'))     	define('__CONTROLLERS_ROOT__'		, $_ROOT     			. 'controllers' .$_SEP);
 	if (!defined('__CONTROLLERS_ACTION__'))   	define('__CONTROLLERS_ACTION__'		, __CONTROLLERS_ROOT__  . 'action' .$_SEP);
 	if (!defined('__CONTROLLERS_AJAX__'))     	define('__CONTROLLERS_AJAX__'		, __CONTROLLERS_ROOT__  . 'ajax' .$_SEP);
-	
+
 	if (!defined('__MODELS_ROOT__'))		  	define('__MODELS_ROOT__'			, $_ROOT   				. 'models' .$_SEP);
-	
+
 
 	// Liens relatifs
-	
+
 	$_SEP = '/';
 	$_ROOT = '';					// racine pour include Smarty, Ajax, et URLS
 
@@ -26,13 +30,13 @@
 	if (!defined('__VIEWS_IMG__'))           	define('__VIEWS_IMG__'				, __VIEWS_ROOT__    . 'img' .$_SEP);
 	if (!defined('__VIEWS_LAYOUTS__'))          define('__VIEWS_LAYOUTS__'			, __VIEWS_ROOT__    . 'layouts' .$_SEP);
 	if (!defined('__VIEWS_PAGES__'))         	define('__VIEWS_PAGES__'			, __VIEWS_ROOT__    . 'pages' .$_SEP);
-	
+
 	if (!defined('__WEB_ROOT__')) 				define('__WEB_ROOT__'				, $_ROOT   			. 'web' .$_SEP);
 
 	if (!defined('__WEB_LIB_THEME__')) 			define('__WEB_LIB_THEME__'			, __WEB_ROOT__ 		. 'sbadmin' .$_SEP);
 	if (!defined('__WEB_LIB_BOWER__')) 			define('__WEB_LIB_BOWER__'			, __WEB_LIB_THEME__ . 'bower_components' .$_SEP);
 	if (!defined('__WEB_LIB_BOOTSTRAP__')) 		define('__WEB_LIB_BOOTSTRAP__'		, __WEB_LIB_BOWER__ . 'bootstrap' .$_SEP);
-	
+
 	if (!defined('__WEB_LIB_FORMVALID__')) 		define('__WEB_LIB_FORMVALID__'		, __WEB_ROOT__ 		. 'formvalidation-v0.6.3' .$_SEP);
 	if (!defined('__WEB_LIB_SELECT__')) 		define('__WEB_LIB_SELECT__'			, __WEB_ROOT__	 	. 'select' .$_SEP);
 	if (!defined('__WEB_LIB_SMARTY__')) 		define('__WEB_LIB_SMARTY__'			, __WEB_ROOT__ 		. 'smarty' .$_SEP);
@@ -42,10 +46,10 @@
 	if (!defined('__WEB_LIB_DATASELECT__')) 	define('__WEB_LIB_DATASELECT__'		, __WEB_ROOT__ 		. 'datatables-select' .$_SEP);
 	if (!defined('__WEB_LIB_BOOTBOX__'))        define('__WEB_LIB_BOOTBOX__'        , __WEB_ROOT__      . 'bootbox' .$_SEP);
 	if (!defined('__WEB_LIB_AUTOCPL__'))        define('__WEB_LIB_AUTOCPL__'        , __WEB_ROOT__      . 'jQuery-Autocomplete' .$_SEP);
-	
-	
+
+
 	// Paramètres
-	
+
 	if (!defined('__APP_TITLE__'))            	define('__APP_TITLE__'				, 'JoliJeûne');		//titre appli
 	if (!defined('__APP_VERSION__'))           	define('__APP_VERSION__'			, 'v.1.01');		//version appli
 	if (!defined('__PWD_DEFAULT__')) 			define('__PWD_DEFAULT__'			, 'Joje1234');	    //mot de passe user par défaut
@@ -56,7 +60,7 @@
 	if (!defined('__LANG_DEFAULT__')) 			define('__LANG_DEFAULT__'			, 'fr');			//langue par defaut de l'appli
 	if (!defined('__SQL_LIMIT__'))	 			define('__SQL_LIMIT__'				, ' LIMIT 0, 1000');//limit dans les SELECT
 
-	
+
 	// Detect Device/Operating System
 	$agent = $_SERVER['HTTP_USER_AGENT'];
 
@@ -68,4 +72,4 @@
 	elseif(preg_match('/Unix/i',$agent)) 	define('__OS__', 'Unix');
 	elseif(preg_match('/Windows/i',$agent)) define('__OS__', 'Windows');
 	else define('__OS__', 'Unknown');
-	
+
--- controllers/c_clients_list.php
+++ controllers/c_clients_list.php
@@ -3,21 +3,26 @@
 	$_file_ = __FILE__;
 	include(__CONTROLLERS_ROOT__.'o_listemode.inc.php');
 
-	if ( $_POST['listclient-paramclient'] == '$13' ) {	
+	if ( isset($_POST['listclient-introvalue']) ) {
 
-		$oSmarty->assign('introvalue', trim($_POST['listclient-introvalue']));
-    	$oSmarty->assign('allClients', Clients::getAllIn($pdo));
+	    // l'utilisateur tape entrée : recherche = filtre datatable
+    	if ( $_POST['listclient-paramclient'] == '$13' ) {	
+    
+    		$oSmarty->assign('introvalue', trim($_POST['listclient-introvalue']));
+    	}
+	    // une ligne est cliquée dans la liste AC
+    	else{
+    	    $paramkey = $_POST['listclient-paramclient'];
+    	    $paramname = substr(
+    	        $_POST['listclient-introvalue'],
+    	        0,
+    	        strpos($_POST['listclient-introvalue'], '-')
+    	        );
+    	    
+    	    header("Location: index.php?page=clients&paramkey={$paramkey}&paramname={$paramname}");
+    	}
 	}
-	else{
-	    $paramkey = $_POST['listclient-paramclient'];
-	    $paramname = substr(
-	        $_POST['listclient-introvalue'],
-	        0,
-	        strpos($_POST['listclient-introvalue'], '-')
-	        );
-	    
-	    header("Location: index.php?page=clients&paramkey={$paramkey}&paramname={$paramname}");
-	}
+
+	$oSmarty->assign('allClients', Clients::getAllIn($pdo));
 	
-
 ?>
