6b1b2d8e94c1768ee7f741b99b20c5f54fab4dad
FMBee <33802035+FMBee@users.noreply.github.com>
2020-01-07 08:40:31 +0100

    bug fin

--- controllers/action/a_prestations.php
+++ controllers/action/a_prestations.php
@@ -6,16 +6,17 @@
 		Prestations::delOne($pdo, $_GET['supp_id']);
 	}
 	else {
-debug($_POST);
+//debug($_POST);
 // 		if (! isset( $_POST['pre_prixttc'] ) || $_POST['pre_prixttc'] != '1' )		// checkbox indefini si 0
 // 		{
 // 			$_POST['pre_prixttc'] = '0';
 // 			$_POST['pre_puht'] = '0';
 // 		}
+		$_POST['pre_prixttc'] = '0';
 		$_POST['pre_puttc'] = floatval( $_POST['pre_puht'] ) * ( ( floatval( $_POST['pre_tauxtva']) + 100 ) / 100 );
 
 		$rq = Prestations::majOrAdd($pdo, $_POST);
-debug($_POST);debug($rq);
+//debug($_POST);debug($rq);
 	}
 
 ?>
