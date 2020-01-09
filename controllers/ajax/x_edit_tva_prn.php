57a97966a33a36ffbb38868d866725014afec830
FMBee <33802035+FMBee@users.noreply.github.com>
2020-01-09 08:40:11 +0100

    editTva

--- controllers/ajax/x_edit_tva_prn.php
+++ controllers/ajax/x_edit_tva_prn.php
@@ -19,23 +19,23 @@
 // 	ob_end_clean ();
 
 	require('x_common.inc.php');
-	
+
 	require_once(__MODELS.'m_parametres.php');
 	require_once(__MODELS.'m_factures.php');
 	require_once(__MODELS.'m_reglements.php');
-	
+
 	require(__ROOT .'web/fpdf/pdf_code128.php');
-	
-	
+
+
 	define ( '_HTLG1', 6 );
 	define ( '_HTLG2', 4 );
 	define ( '_STLG', chr(10) );
 	define ( '_EUR', '' );
-	
+
 	$dates = explode(':', trim($_GET['paramedit']));
 	$debut = date_format(date_create_from_format('d-m-Y', $dates[0]), 'Y/m/d');
 	$fin   = date_format(date_create_from_format('d-m-Y', $dates[1]), 'Y/m/d');
-	
+
 	$params = Parametres::getOne($pdo, '1');
 	$ligneTTC = Factures::getComptaTtc($pdo, $debut, $fin);
 	$ligneHT  = Factures::getComptaHt($pdo, $debut, $fin);
@@ -43,14 +43,14 @@
 	$ligneREG = Reglements::getComptaList($pdo, $debut, $fin);
 
 	$filename = "EditionCompta.pdf";
-	
-	
+
+
 	//******** entête
-	
-	$pdf = new PDF_Code128 ();
+
+	$pdf = new C_PDF_Code128 ();
 	$pdf->SetAutoPageBreak ( true, 10 );
 	$pdf->AddPage();
-	
+
 	$pdf->SetFont('Arial','B',14);
 	$pdf->Cell(60,_HTLG2,utf8_decode( $params['par_societe']), 0, 1);
 	$pdf->ln();
@@ -65,72 +65,72 @@
 			'IBAN: '.$params['par_iban']._STLG.
 			'TVA Intracom: '.$params['par_TvaIntracom'];
 	$pdf->MultiCell(90, _HTLG2, utf8_decode( $var ), 0, 'L' );
-	
+
 	$pdf->SetY(60);
 	$pdf->SetFont('Arial','',12);
 	$var =  "COMPTABILISATION DES FACTURES ET REGLEMENTS"._STLG.
 			"Période du ".$dates[0].' au '.$dates[1].'         / Edité le '.date('d-m-Y');
 	$pdf->MultiCell(170, _HTLG1, utf8_decode( $var ), 0, 'C' );
-	
-/*	
- * $ligneTTC=Array ( 
- * [0] => Array ( [_totalttc] => 17099 ) ) 
- * $ligneHT=Array ( 
- * [0] => Array ( [pre_compteht] => 706000 [_totalht] => 3725 ) 
- * [1] => Array ( [pre_compteht] => 706100 [_totalht] => 833.3299999999999 ) 
- * [2] => Array ( [pre_compteht] => 706110 [_totalht] => 1088.33 ) 
- * $ligneTVA=Array ( ) Array ( 
- * [0] => Array ( [pre_comptetva] => 445710 [_totaltva] => 1434.8600000000001 ) 
- * [1] => Array ( [pre_comptetva] => 445730 [_totaltva] => 332.72999999999985 ) 
- * [2] => Array ( [pre_comptetva] => 447530 [_totaltva] => 439.07000000000016 ) ) 
- * $ligneREG=Array ( 
- * [0] => Array ( [reg_remise] => 2015-08-10 00:00:00 [reg_mode] => CB [_total] => 240 ) 
- * [1] => Array ( [reg_remise] => 2015-08-10 00:00:00 [reg_mode] => chq [_total] => 3586 ) 
- * [2] => Array ( [reg_remise] => 2015-08-10 00:00:00 [reg_mode] => vir [_total] => 1000 ) 
- * [3] => Array ( [reg_remise] => 2015-08-12 00:00:00 [reg_mode] => chq [_total] => 380 ) 
- * [4] => Array ( [reg_remise] => 2015-08-14 00:00:00 [reg_mode] => chq [_total] => 200 ) 
- * [5] => Array ( [reg_remise] => 2015-08-15 00:00:00 [reg_mode] => chq [_total] => -200 ) 
+
+/*
+ * $ligneTTC=Array (
+ * [0] => Array ( [_totalttc] => 17099 ) )
+ * $ligneHT=Array (
+ * [0] => Array ( [pre_compteht] => 706000 [_totalht] => 3725 )
+ * [1] => Array ( [pre_compteht] => 706100 [_totalht] => 833.3299999999999 )
+ * [2] => Array ( [pre_compteht] => 706110 [_totalht] => 1088.33 )
+ * $ligneTVA=Array ( ) Array (
+ * [0] => Array ( [pre_comptetva] => 445710 [_totaltva] => 1434.8600000000001 )
+ * [1] => Array ( [pre_comptetva] => 445730 [_totaltva] => 332.72999999999985 )
+ * [2] => Array ( [pre_comptetva] => 447530 [_totaltva] => 439.07000000000016 ) )
+ * $ligneREG=Array (
+ * [0] => Array ( [reg_remise] => 2015-08-10 00:00:00 [reg_mode] => CB [_total] => 240 )
+ * [1] => Array ( [reg_remise] => 2015-08-10 00:00:00 [reg_mode] => chq [_total] => 3586 )
+ * [2] => Array ( [reg_remise] => 2015-08-10 00:00:00 [reg_mode] => vir [_total] => 1000 )
+ * [3] => Array ( [reg_remise] => 2015-08-12 00:00:00 [reg_mode] => chq [_total] => 380 )
+ * [4] => Array ( [reg_remise] => 2015-08-14 00:00:00 [reg_mode] => chq [_total] => 200 )
+ * [5] => Array ( [reg_remise] => 2015-08-15 00:00:00 [reg_mode] => chq [_total] => -200 )
  */
 
 	//********* tableau synthese tva
-	
+
 	$pdf->ln(2);
 	$var =  "RESUME DES COMPTES";
 	$pdf->MultiCell(170, _HTLG1, utf8_decode( $var ), 0, 'L' );
-	
+
 	$contenuTableau = [];
-	
+
 	foreach ($ligneTTC as $ligne) {
-	
+
 		$data = [];
 		$data[] = utf8_decode('[L]'.'411000');
 		$data[] = utf8_decode('[L]'.'Factures clients');
 		$data[] = utf8_decode('[R]'.sprintf('%.2f', $ligne['_totalttc'])._EUR);
 		$data[] = utf8_decode('[R]'.''._EUR);
-	
+
 		$contenuTableau = array_merge( $contenuTableau, $data);
 	}
 	foreach ($ligneHT as $ligne) {
-	
+
 		$data = [];
 		$data[] = utf8_decode('[L]'.$ligne['pre_compteht']);
 		$data[] = utf8_decode('[L]'.'Hors taxes');
 		$data[] = utf8_decode('[R]'.''._EUR);
 		$data[] = utf8_decode('[R]'.sprintf('%.2f', $ligne['_totalht'])._EUR);
-	
+
 		$contenuTableau = array_merge( $contenuTableau, $data);
 	}
 	foreach ($ligneTVA as $ligne) {
-	
+
 		$data = [];
 		$data[] = utf8_decode('[L]'.$ligne['pre_comptetva']);
 		$data[] = utf8_decode('[L]'.'TVA');
 		$data[] = utf8_decode('[R]'.''._EUR);
 		$data[] = utf8_decode('[R]'.sprintf('%.2f', $ligne['_totaltva'])._EUR);
-	
+
 		$contenuTableau = array_merge( $contenuTableau, $data);
 	}
-	
+
 	$contenuHeader = array(
 			20, 100, 35, 35, 		// total 190 +(2*10)
 			'[L]'.utf8_decode('Compte'),
@@ -176,34 +176,34 @@
 			'BRD_TYPE' 		=> 'LR',
 			'BRD_TYPE_NEW_PAGE' => '',
 	);
-	
+
 	$pdf->drawTableau($pdf, $proprietesTableau, $proprieteHeader, $contenuHeader, $proprieteContenu, $contenuTableau);
 
-	
+
 	//********** tableau reglements
-	
+
 	$pdf->ln();
 	$var =  "RESUME DES REGLEMENTS PAR MODE";
 	$pdf->MultiCell(170, _HTLG1, utf8_decode( $var ), 0, 'L' );
-	
+
 	$contenuTableau = [];
 	$totalgen = 0;
-	
+
 // 	* [4] => Array ( [reg_remise] => 2015-08-14 00:00:00 [reg_mode] => chq [_total] => 200 )
-	
+
 	foreach ($ligneREG as $ligne) {
-	
+
 		$data = [];
 		$data[] = utf8_decode('[L]'.date_format( date_create($ligne['reg_remise']), 'd/m/Y'));
 		$data[] = utf8_decode('[L]'.$ligne['reg_mode']);
 		$data[] = utf8_decode('[R]'.sprintf('%.2f', $ligne['_total'])._EUR);
 		$totalgen += $ligne['_total'];
-	
+
 		$contenuTableau = array_merge( $contenuTableau, $data);
 	}
-	
+
 	$contenuHeader = array(
-			30, 40, 30, 
+			30, 40, 30,
 			'[C]'.utf8_decode('Remise'),
 			'[C]'.utf8_decode('Mode'),
 			'[C]'.utf8_decode('Montant'),
@@ -246,17 +246,17 @@
 			'BRD_TYPE' 		=> 'LR',
 			'BRD_TYPE_NEW_PAGE' => '',
 	);
-	
+
 	$pdf->drawTableau($pdf, $proprietesTableau, $proprieteHeader, $contenuHeader, $proprieteContenu, $contenuTableau);
 	$pdf->Cell(70, _HTLG1, '', 0, 0);
 	$pdf->Cell(30, _HTLG1, utf8_decode(sprintf('%.2f', $totalgen)._EUR), 1, 0, 'R' );
-	
-/*	
+
+/*
 */
 // 	$pdf->Output(__ROOT .'views/factures/'.$filename, 'F');
 // 	$pdf->Output('D');
 	$pdf->Output( $filename, 'I');
-	
+
 // 	$pdf->AutoPrint ( true );
 
 
